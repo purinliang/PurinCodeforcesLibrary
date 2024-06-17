50296ed271a0bd8e2799e634cd7bbdfcf470931e
purinliang <purinliang@gmail.com>
2024-06-17 21:56:10 +0800

    Create 标准模板_StandardTemplate.cpp

---
+++ 工具_Tools/比赛模板_CompetitionTemplate/标准模板_StandardTemplate.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+/* DEBUG AND ASSERT*/
+
+#ifdef LOCAL
+#include ".\LOCAL.h"
+#else
+#define D(...)
+#define DN(arr, n)
+#define DF()
+#define ASSERT(x)
+#endif
+
+/* MY CODE BEGIN */
+
+const int INF = 0x3F3F3F3F;
+const ll LINF = 0x3F3F3F3F3F3F3F3FLL;
+
+void purin_init () {
+    cout << fixed << setprecision (12);
+    ios::sync_with_stdio (false);
+    cin.tie (nullptr);
+    cout.tie (nullptr);
+}
+
+void purin_solve () {
+}
+
+/* MY CODE END */
+
+int main () {
+    purin_init ();
+#ifdef LOCAL
+    local_multi_case ();
+#else
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; ++i) {
+        purin_solve ();
+    }
+#endif  // LOCAL
+    return 0;
+}
