1cb23037f724d603a9b806d746c7d9de600b82c4
purinliang <purinliang@gmail.com>
2024-06-19 09:00:49 +0800

    Update 标准模板_StandardTemplate.cpp

--- 工具_Tools/比赛模板_CompetitionTemplate/标准模板_StandardTemplate.cpp
+++ 工具_Tools/比赛模板_CompetitionTemplate/标准模板_StandardTemplate.cpp
@@ -9,59 +9,60 @@ typedef long long ll;
 #define DN(arr, n)
 #define DF()
 #define ASSERT(x)
-#endif
 
 template <typename T>
-void _RD (T& var) {
+void _RD(T& var) {
     cin >> var;
 }
 
-void _RD (char* var) {
+void _RD(char* var) {
     cin >> (var + 1);
 }
 
-void RD () {
+void RD() {
 }
 
 template <typename T, typename... U>
-void RD (T& Head, U&... Tail) {
-    _RD (Head);
-    RD (Tail...);
+void RD(T& Head, U&... Tail) {
+    _RD(Head);
+    RD(Tail...);
 }
 
 template <typename T>
-void RDN (T* arr, int n) {
+void RDN(T* arr, int n) {
     for (int i = 1; i <= n; ++i) {
-        _RD (arr[i]);
+        _RD(arr[i]);
     }
 }
 
 template <typename T>
-void _WT (const T& var) {
+void _WT(const T& var) {
     cout << var;
 }
 
-void _WT (const char* var) {
+void _WT(const char* var) {
     cout << (var + 1);
 }
 
-void WT () {
+void WT() {
 }
 
 template <typename T, typename... U>
-void WT (const T& Head, const U&... Tail) {
-    _WT (Head);
+void WT(const T& Head, const U&... Tail) {
+    _WT(Head);
     cout << (sizeof...(Tail) ? " " : "\n");
-    WT (Tail...);
+    WT(Tail...);
 }
 
 template <typename T>
-void WTN (T* arr, int n) {
+void WTN(T* arr, int n) {
     for (int i = 1; i <= n; ++i) {
-        _WT (arr[i]);
+        _WT(arr[i]);
         cout << (i < n ? " " : "\n");
     }
 }
+
+#endif
 
 /* MY CODE BEGIN */
 
@@ -69,36 +70,37 @@ const int INF = 0x3F3F3F3F;
 const ll LINF = 0x3F3F3F3F3F3F3F3FLL;
 const int MAXN = 3e5 + 10;
 
-void purin_init () {
-    cout << fixed << setprecision (12);
-    ios::sync_with_stdio (false);
-    cin.tie (nullptr);
-    cout.tie (nullptr);
+void purin_init() {
+    cout << fixed << setprecision(12);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
 #define endl "\n"
-#define fout fflush (stdout)
+#define fout fflush(stdout)
 }
 
 int n;
 int a[MAXN];
 
-void purin_solve () {
-    RD (n);
-    RDN (a, n);
-    D (n);
-    DN (a, n);
-    // WT (n);
-    // WTN (a, n);
+void purin_solve() {
+    RD(n);
+    RDN(a, n);
+
+    D(n);
+    DN(a, n);
+    WT(n);
+    WTN(a, n);
 }
 
-int main () {
-    purin_init ();
+int main() {
+    purin_init();
 #ifdef LOCAL
-    purin_local_multi_case ();
+    purin_local_multi_case();
 #else
     int t = 1;
     cin >> t;
     for (int i = 1; i <= t; ++i) {
-        purin_solve ();
+        purin_solve();
     }
 #endif
     return 0;
