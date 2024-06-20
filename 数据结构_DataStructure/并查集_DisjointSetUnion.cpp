689ebea240edcecd39c7f71bc63aa631f62b811b
purinliang <purinliang@gmail.com>
2024-06-20 18:06:35 +0800

    Update 并查集_DisjointSetUnion.cpp
    重命名数据结构

--- 数据结构_DataStructure/并查集_DisjointSetUnion.cpp
+++ 数据结构_DataStructure/并查集_DisjointSetUnion.cpp
@@ -3,42 +3,42 @@ using namespace std;
 typedef long long ll;
 
 struct DisjointSetUnion {
-    vector<int> fnd, siz;
+    vector<int> _prt, _siz;
 
-    void Init(int n) {
-        fnd.clear(), fnd.resize(n + 2);
-        siz.clear(), siz.resize(n + 2);
+    void init(int n) {
+        _prt.clear(), _prt.resize(n + 2);
+        _siz.clear(), _siz.resize(n + 2);
         for (int i = 1; i <= n; i++) {
-            fnd[i] = i, siz[i] = 1;
+            _prt[i] = i, _siz[i] = 1;
         }
     }
 
-    int Find(int x) {
+    int find(int x) {
         int rt = x;
-        while (fnd[rt] != rt) {
-            rt = fnd[rt];
+        while (_prt[rt] != rt) {
+            rt = _prt[rt];
         }
-        int fx = fnd[x];
-        while (fx != rt) {
-            fnd[x] = rt;
-            x = fx, fx = fnd[x];
+        int prtx = _prt[x];
+        while (prtx != rt) {
+            _prt[x] = rt;
+            x = prtx, prtx = _prt[x];
         }
         return rt;
     }
 
-    bool Same(int x, int y) {
-        return Find(x) == Find(y);
+    bool same(int x, int y) {
+        return find(x) == find(y);
     }
 
-    bool Merge(int x, int y) {
-        int rx = Find(x), ry = Find(y);
+    bool merge(int x, int y) {
+        int rx = find(x), ry = find(y);
         if (rx == ry) {
             return false;
         }
-        if (siz[rx] < siz[ry]) {
+        if (_siz[rx] < _siz[ry]) {
             swap(rx, ry);
         }
-        fnd[ry] = rx, siz[rx] += siz[ry];
+        _prt[ry] = rx, _siz[rx] += _siz[ry];
         return true;
     }
 
