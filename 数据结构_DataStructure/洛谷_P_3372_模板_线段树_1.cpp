0b364470a2a358d122d4cc96d857ab9305b3697a
purinliang <purinliang@gmail.com>
2024-06-20 12:38:51 +0800

    Update 线段树

--- 数据结构_DataStructure/洛谷_P_3372_模板_线段树_1.cpp
+++ 数据结构_DataStructure/洛谷_P_3372_模板_线段树_1.cpp
@@ -2,6 +2,115 @@
 using namespace std;
 typedef long long ll;
 
+#ifdef LOCAL
+#include ".\标准本地调试_StandardLocalDebug.h"
+#else
+#define D(...)
+#define DN(arr, n)
+#define DF()
+#define ASSERT(x)
+
+template <typename T>
+void _RD(T& var) {
+    cin >> var;
+}
+
+void _RD(char* var) {
+    cin >> (var + 1);
+}
+
+void RD() {
+}
+
+template <typename T, typename... U>
+void RD(T& Head, U&... Tail) {
+    _RD(Head);
+    RD(Tail...);
+}
+
+template <typename T>
+void RDN(T* arr, int n) {
+    for (int i = 1; i <= n; ++i) {
+        _RD(arr[i]);
+    }
+}
+
+template <typename T>
+void _WT(const T& var) {
+    cout << var;
+}
+
+void _WT(const char* var) {
+    cout << (var + 1);
+}
+
+void WT() {
+}
+
+template <typename T, typename... U>
+void WT(const T& Head, const U&... Tail) {
+    _WT(Head);
+    cout << (sizeof...(Tail) ? " " : "\n");
+    WT(Tail...);
+}
+
+template <typename T>
+void WTN(T* arr, int n) {
+    for (int i = 1; i <= n; ++i) {
+        _WT(arr[i]);
+        cout << (i < n ? " " : "\n");
+    }
+}
+
+#endif
+
+void purin_ios() {
+    cout << fixed << setprecision(12);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#define endl "\n"
+    // fflush(stdout);
+}
+
+void purin_init();
+void purin_solve();
+
+void purin_online_test(bool ignore_test_case_count) {
+    purin_ios();
+    purin_init();
+    if (!ignore_test_case_count) {
+        int t = 1;
+        cin >> t;
+        for (int i = 1; i <= t; ++i) {
+            purin_solve();
+        }
+    } else {
+        auto cin_eof = [&]() -> bool {
+            char ch;
+            while (cin >> ch) {
+                if (!isspace(ch)) {
+                    cin.unget();
+                    return false;
+                }
+            }
+            return cin.eof();
+        };
+        while (!cin_eof()) {
+            purin_solve();
+        }
+    }
+}
+
+/* MY CODE BEGIN */
+
+const int INF = 0x3F3F3F3F;
+const ll LINF = 0x3F3F3F3F3F3F3F3FLL;
+const int MAXN = 3e5 + 10;
+
+void purin_init() {
+}
+
 struct SegmentTree {
     // #define USING_MIN_MAX
    private:
@@ -239,3 +348,39 @@ struct SegmentTree {
 #undef USING_MIN_MAX
 
 } st;
+
+int n, m;
+int a[MAXN];
+
+void purin_solve() {
+    RD(n, m);
+    RDN(a, n);
+    DN(a, n);
+    st.Build(n, a);
+    st.show();
+    while (m--) {
+        int opt;
+        RD(opt);
+        if (opt == 1) {
+            int l, r, k;
+            RD(l, r, k);
+            st.Add(l, r, k);
+            st.show();
+        } else if (opt == 2) {
+            int l, r;
+            RD(l, r);
+            ll res = st.Sum(l, r);
+            WT(res);
+        }
+    }
+}
+
+int main() {
+    const int IGNORE_TEST_CASE_COUNT = true;
+#ifdef LOCAL
+    purin_local_test(IGNORE_TEST_CASE_COUNT);
+#else
+    purin_online_test(IGNORE_TEST_CASE_COUNT);
+#endif
+    return 0;
+}
