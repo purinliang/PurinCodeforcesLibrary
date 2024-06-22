962853e461f40c9997d711f6adb467451c6f9550
purinliang <purinliang@gmail.com>
2024-06-23 04:06:39 +0800

    Update 组合数学 定义求组合数

--- 数学_Mathematics/组合数学_Combinatorics.cpp
+++ 数学_Mathematics/组合数学_Combinatorics.cpp
@@ -4,13 +4,15 @@ typedef long long ll;
 
 namespace Combinatorics {
 
+// TODO: 复制模整数类进来，用这个类通过了的提交可以标上日期，贴在下面（不是贴题目）
+
 // MOD must be a very big prime (e.g. at least 1e6) and bigger than max(n)
-static const int MOD = 998244353;
+// static const int MOD = 998244353;
 
 ll qpow(ll x, ll n) {
     ll res = 1;
     for (; n; n >>= 1) {
-        if (n & 1) {
+        if (n & 1LL) {
             res = res * x % MOD;
         }
         x = x * x % MOD;
@@ -39,17 +41,35 @@ void _init(int n) {
     }
 }
 
+ll A_slow(ll n, ll m) {
+    const int MAX_M = 1e6 + 10;
+    assert(m <= MAX_M);
+    ll res = 1LL;
+    for (int i = 1; i <= m; ++i) {
+        res = 1LL * res * (n + 1 - i) % MOD;
+    }
+    return res;
+}
+
+ll C_slow(ll n, ll m) {
+    m = min(n, n - m);
+    const int MAX_M = 1e6 + 10;
+    assert(m <= MAX_M);
+    _init(m);
+    return 1LL * A_slow(n, m) * inv_fac[m] % MOD;
+}
+
 ll A(ll n, ll m) {
-    if (n < 0 || m < 0 || n < m) {
-        return 0;
+    if (n < 0LL || m < 0LL || n < m) {
+        return 0LL;
     }
     _init(n);
     return 1LL * fac[n] * inv_fac[n - m] % MOD;
 }
 
 ll C(ll n, ll m) {
-    if (n < 0 || m < 0 || n < m) {
-        return 0;
+    if (n < 0LL || m < 0LL || n < m) {
+        return 0LL;
     }
     _init(n);
     return 1LL * A(n, m) * inv_fac[m] % MOD;
