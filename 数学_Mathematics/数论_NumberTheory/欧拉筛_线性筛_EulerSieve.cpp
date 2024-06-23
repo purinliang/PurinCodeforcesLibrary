2236ddce1f90cf7d60a2556fff1db3fb5e4e6931
purinliang <purinliang@gmail.com>
2024-06-23 21:55:52 +0800

    Update 欧拉筛 改为继承方式 新增对某个数的所有因数和质因数求值的方法

--- 数学_Mathematics/数论_NumberTheory/欧拉筛_线性筛_EulerSieve.cpp
+++ 数学_Mathematics/数论_NumberTheory/欧拉筛_线性筛_EulerSieve.cpp
@@ -2,97 +2,116 @@
 using namespace std;
 typedef long long ll;
 
-namespace EulerSieveMinPrime {
+struct BaseSieve {
+    vector<int> p;
 
-vector<int> p, min_p, min_p_k;
-
-string to_string(int lim) {
-    string p_str = "p = [";
-    for (int j = 1; j < p.size() && j <= lim; ++j) {
-        p_str += std::to_string(p[j]) + ", ";
-    }
-    p_str = p_str.substr(0, max(0, (int)p_str.length() - 2)) + "]";
-    // return p_str;
-    string min_p_str = "min_p = [";
-    for (int j = 1; j <= lim; ++j) {
-        min_p_str += std::to_string(min_p[j]) + ", ";
-    }
-    min_p_str = min_p_str.substr(0, max(0, (int)min_p_str.length() - 2)) + "]";
-    string min_p_k_str = "min_p_k = [";
-    for (int j = 1; j <= lim; ++j) {
-        min_p_k_str += std::to_string(min_p_k[j]) + ", ";
-    }
-    min_p_k_str = min_p_k_str.substr(0, max(0, (int)min_p_k_str.length() - 2)) + "]";
-    return p_str + ",\n" + min_p_str + ",\n" + min_p_k_str;
-}
-
-void show() {
-#ifdef LOCAL
-    cout << "[D] " << to_string(20) << endl;
-#endif
-}
-
-void euler_sieve(int n) {
-    p.clear();
-    min_p.clear(), min_p.resize(n + 2);
-    min_p_k.clear(), min_p_k.resize(n + 2);
-    for (int i = 1; i <= n; ++i) {
-        if (min_p[i] == 0) {
-            p.push_back(i);
-            min_p[i] = i;
-            min_p_k[i] = i;
-        }
-        for (int j = 1, t; j < p.size() && (t = i * p[j]) <= n; ++j) {
-            min_p[t] = p[j];
-            if (i % p[j] == 0) {
-                min_p_k[t] = min_p_k[i] * p[j];
+    string to_string(int lim) {
+        string res = "p = [";
+        for (const auto& d : p) {
+            if (d > lim) {
                 break;
-            } else {
-                min_p_k[t] = p[j];
+            }
+            res += std::to_string(d) + ", ";
+        }
+        res = res.substr(0, max(0, (int)res.length() - 2)) + "]";
+        return res;
+    }
+
+    void show() {
+#ifdef LOCAL
+        cout << "[D] " << to_string(20) << endl;
+#endif
+    }
+
+    template <typename function>
+    void calc_d(ll x, bool skip_1, function f) {
+        for (const auto& d : p) {
+            if (d * d > x) {
+                break;
+            }
+            if (x % d != 0) {
+                continue;
+            }
+            ll d1 = (d == 1) ? (skip_1 ? 0LL : d) : d;
+            if (d1 > 0LL) {
+                f(d1);
+            }
+            ll d2 = (d * d == x) ? 0LL : x / d;
+            if (d2 > 0LL) {
+                f(d2);
             }
         }
     }
+};
 
-    show();
-}
+struct EulerSieveMinPrime : public BaseSieve {
+    vector<int> min_p, min_p_k;
 
-}  // namespace EulerSieveMinPrime
-
-namespace EulerSieveNotPrime {
-
-vector<int> p;
-vector<bool> not_p;
-
-string to_string(int lim) {
-    string res = "p = [";
-    for (int j = 1; j < p.size() && j <= lim; ++j) {
-        res += std::to_string(p[j]) + ", ";
-    }
-    res = res.substr(0, max(0, (int)res.length() - 2)) + "]";
-    return res;
-}
-
-void show() {
-#ifdef LOCAL
-    cout << "[D] " << to_string(20) << endl;
-#endif
-}
-
-void euler_sieve(int n) {
-    p.clear();
-    not_p.clear(), not_p.resize(n + 2);
-    for (int i = 1; i <= n; ++i) {
-        if (not_p[i] == 0) {
-            p.push_back(i);
+    string to_string(int lim) {
+        string p_str = "p = [";
+        for (int j = 1; j < p.size() && j <= lim; ++j) {
+            p_str += std::to_string(p[j]) + ", ";
         }
-        for (int j = 1, t; j < p.size() && (t = i * p[j]) <= n; ++j) {
-            not_p[t] = true;
-            if (i % p[j] == 0) {
-                break;
+        p_str = p_str.substr(0, max(0, (int)p_str.length() - 2)) + "]";
+        // return p_str;
+        string min_p_str = "min_p = [";
+        for (int j = 1; j <= lim; ++j) {
+            min_p_str += std::to_string(min_p[j]) + ", ";
+        }
+        min_p_str = min_p_str.substr(0, max(0, (int)min_p_str.length() - 2)) + "]";
+        string min_p_k_str = "min_p_k = [";
+        for (int j = 1; j <= lim; ++j) {
+            min_p_k_str += std::to_string(min_p_k[j]) + ", ";
+        }
+        min_p_k_str = min_p_k_str.substr(0, max(0, (int)min_p_k_str.length() - 2)) + "]";
+        return p_str + ",\n" + min_p_str + ",\n" + min_p_k_str;
+    }
+
+    void init(int n) {
+        p.clear();
+        min_p.clear(), min_p.resize(n + 2);
+        min_p_k.clear(), min_p_k.resize(n + 2);
+        for (int i = 1; i <= n; ++i) {
+            if (min_p[i] == 0) {
+                p.push_back(i);
+                min_p[i] = i;
+                min_p_k[i] = i;
+            }
+            for (int j = 1, t; j < p.size() && (t = i * p[j]) <= n; ++j) {
+                min_p[t] = p[j];
+                if (i % p[j] == 0) {
+                    min_p_k[t] = min_p_k[i] * p[j];
+                    break;
+                } else {
+                    min_p_k[t] = p[j];
+                }
             }
         }
-    }
 
-    show();
-}
-}  // namespace EulerSieveNotPrime
+        show();
+    }
+};
+
+struct EulerSieveNotPrime : public BaseSieve {
+    vector<bool> not_p;
+
+    void init(int n) {
+        p.clear();
+        not_p.clear(), not_p.resize(n + 2);
+        for (int i = 1; i <= n; ++i) {
+            if (not_p[i] == 0) {
+                p.push_back(i);
+            }
+            for (int j = 1, t; j < p.size() && (t = i * p[j]) <= n; ++j) {
+                not_p[t] = true;
+                if (i % p[j] == 0) {
+                    break;
+                }
+            }
+        }
+
+        show();
+    }
+};
+
+EulerSieveNotPrime euler_sieve;
