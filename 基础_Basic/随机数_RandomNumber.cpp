d0c4865bafff65d2ac3a3309fb018350de60a64b
purinliang <purinliang@gmail.com>
2024-07-01 19:46:12 +0800

    Update 随机数_RandomNumber.cpp

--- 基础_Basic/随机数_RandomNumber.cpp
+++ 基础_Basic/随机数_RandomNumber.cpp
@@ -7,7 +7,7 @@ typedef unsigned long long ull;
 
 /* RANDOM */
 
-#define TIME chrono::system_clock::now ().time_since_epoch ().count ()
+#define TIME chrono::system_clock::now().time_since_epoch().count()
 
 #ifdef LOCAL
 const int rnd_seed = 19937;
@@ -15,19 +15,19 @@ const int rnd_seed = 19937;
 const int rnd_seed = TIME;
 #endif  // LOCAL
 
-mt19937 rnd (rnd_seed);
+mt19937 rnd(rnd_seed);
 
 // Generate an signed int between [L, R]
-int range_rand (int L, int R) {
-    assert (L <= R);
-    int res = (int)((1.0 * rnd () / UINT_MAX) * (R - L + 1)) + L;
+int range_rand(int L, int R) {
+    assert(L <= R);
+    int res = (int)((1.0 * rnd() / UINT_MAX) * (R - L + 1)) + L;
     return res;
 }
 
 // Generate an int between [0, R)
-int range_rand (int R) {
-    assert (R > 0);
-    return range_rand (0, R - 1);
+int range_rand(int R) {
+    assert(R > 0);
+    return range_rand(0, R - 1);
 }
 
 // shuffle(a + 1, a + 1 + n, rnd);
