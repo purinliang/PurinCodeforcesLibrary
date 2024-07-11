584daa25e1bf1e15f3c42fe5c2f42769e0d4437d
purinliang <purinliang@gmail.com>
2024-07-11 17:15:18 +0800

    Update 洛谷_P_3808_AC_自动机_简单版.cpp

--- 字符串_String/洛谷_P_3808_AC_自动机_简单版.cpp
+++ 字符串_String/洛谷_P_3808_AC_自动机_简单版.cpp
@@ -115,11 +115,11 @@ void purin_online_test(bool ignore_test_case_count) {
 const int INF = 0x3F3F3F3F;
 const ll LINF = 0x3F3F3F3F3F3F3F3FLL;
 
-const int MAXN = 2e6 + 10;
+const int MAXN = 1e6 + 10;
 int n;
 char s[MAXN];
 
-struct AhoCorasickAutomatonSimpleVersion {
+struct AhoCorasickAutomaton {
     struct Node {
         int ch[26], fail;
         vector<int> fail_ch;
@@ -202,7 +202,24 @@ struct AhoCorasickAutomatonSimpleVersion {
         }
     }
 
-    void query(char* t) {
+    /**
+     * 清除上一次访问对AC自动机的影响
+     * 针对 query1 类型的影响，可以再次遍历文本串 T 将其清空。
+     * 针对 query2 类型的影响，由于进行了 dp_on_fail_tree，
+     * 所以要将所有相关节点清空，为了图方便直接整个自动机清空。
+     */
+    void clear_cnt() {
+        for (int i = 0; i < node.size(); ++i) {
+            // 清空上次查询的影响，注意 dp_on_fail_tree 污染了其他节点
+            node[i].cnt = 0;
+        }
+    }
+
+    /**
+     * 将文本串 T 输入AC自动机，统计每个节点的访问次数。也就是
+     * 每个节点作为最长真后缀的出现次数。
+     */
+    void query1(char* t) {
         int u = root;
         for (int i = 1; t[i]; ++i) {
             u = node[u].ch[t[i] - 'a'];
@@ -211,44 +228,79 @@ struct AhoCorasickAutomatonSimpleVersion {
     }
 
     /**
-     * 查询每种模式串 S_id 在文本串 T 中是否出现
+     * 将文本串 T 输入AC自动机，统计每个节点的出现次数。也就是
+     * 每个节点作为子串的出现次数。
      */
-    void answer(char* t) {
-        query(t);
+    void query2(char* t) {
+        query1(t);
         dp_on_fail_tree(root);
+    }
+
+    /**
+     * 查询并输出一共有多少种模式串 S_id 在文本串 T 中出现
+     */
+    void answer1(char* t) {
+        query2(t);
 
         int ans = 0;
         for (auto [id, u] : pattern_id_to_node_id) {
-            if (node[u].cnt) {
-                ++ans;
-            }
+            ans += (node[u].cnt > 0);
         }
         cout << ans << endl;
 
-        for (int i = 0; i < node.size(); ++i) {
-            // 清空本次查询的影响，注意 dp_on_fail_tree 污染了其他节点
-            node[i].cnt = 0;
-        }
+        clear_cnt();
     }
 
-} acamsv;
+    /**
+     * 查询哪种模式串 S_id 在文本串 T 中出现次数最多，返回他们的编号和出现次数
+     */
+    pair<vector<int>, int> answer2(char* t) {
+        query2(t);
+
+        vector<int> res1;
+        int res2 = 0;
+        for (auto [id, u] : pattern_id_to_node_id) {
+            if (node[u].cnt >= res2) {
+                if (node[u].cnt > res2) {
+                    res1.clear();
+                }
+                res1.push_back(id);
+                res2 = node[u].cnt;
+            }
+        }
+
+        clear_cnt();
+        return {res1, res2};
+    }
+
+    /**
+     * 查询并输出每种模式串 S_id 在文本串 T 中出现的次数
+     */
+    void answer3(char* t) {
+        query2(t);
+
+        for (auto [id, u] : pattern_id_to_node_id) {
+            cout << node[u].cnt << endl;
+        }
+
+        clear_cnt();
+    }
+
+} acam;
 
 void purin_init() {}
 
 void purin_solve() {
     RD(n);
-    D(n);
-    acamsv.init();
+    acam.init();
     for (int i = 1; i <= n; ++i) {
         RD(s);
-        D(s);
-        acamsv.insert(s, i);
+        acam.insert(s, i);
     }
-    acamsv.build();
-    acamsv.build_fail_tree();
+    acam.build();
+    acam.build_fail_tree();
     RD(s);
-    D(s);
-    acamsv.answer(s);
+    acam.answer1(s);
 }
 
 int main() {
