dbb39884f2f5371811effb52aa97929cad2b1e7f
purinliang <purinliang@gmail.com>
2024-07-12 23:10:27 +0800

    Update 洛谷_P_3804_模板_后缀自动机_SAM.cpp

--- 字符串_String/洛谷_P_3804_模板_后缀自动机_SAM.cpp
+++ 字符串_String/洛谷_P_3804_模板_后缀自动机_SAM.cpp
@@ -120,27 +120,21 @@ int n;
 char s[MAXN];
 
 struct SuffixAutomaton {
+    static const int DEFAULT_STRLEN = 1e6;
     struct Node {
         int len = 0, link = 0;
         int ch[26] = {};
         ll cnt = 0;
-        Node() {};
-        void CopyNewNode(const int& l, const Node& n) {
-            len = l, link = n.link;
-            memcpy(ch, n.ch, sizeof(ch));
-            // cnt = 0;
-        }
     };
 
     vector<Node> node;
-    int last;  // last末尾节点的位置
+    int root, last;  // last末尾节点的位置
 
-    void init() {
-        node.clear();
+    void init(int strlen = DEFAULT_STRLEN) {
+        node.clear(), node.reserve(2 * strlen + 2);
         node.push_back(Node());
-        node.push_back(Node());
-        node[1] = Node();
-        last = 1;
+        root = new_node();
+        last = root;
     }
 
     int new_node() {
@@ -149,54 +143,61 @@ struct SuffixAutomaton {
         return idx;
     }
 
+    int clone_new_node(int clone_from) {
+        int idx = new_node();
+        node[idx].link = node[clone_from].link;
+        memcpy(node[idx].ch, node[clone_from].ch, sizeof(node[clone_from].ch));
+        return idx;
+    }
+
     void extend(char ch) {
         int p = last, u = new_node();
         last = u;
         node[u] = Node();
         node[u].len = node[p].len + 1;
-        node[u].cnt = 1;
-        for (; p && !node[p].ch[ch - 'a']; p = node[p].link)
+        node[u].cnt = 1LL;
+        while (p > 0 && !node[p].ch[ch - 'a']) {
             node[p].ch[ch - 'a'] = u;
-        if (!p)
-            node[u].link = 1;
-        else {
-            int q = node[p].ch[ch - 'a'];
-            if (node[p].len + 1 == node[q].len)
-                node[u].link = q;
-            else {
-                int y = new_node();
-                node[y].CopyNewNode(node[p].len + 1, node[q]);
-                node[q].link = node[u].link = y;
-                for (; p && node[p].ch[ch - 'a'] == q; p = node[p].link)
-                    node[p].ch[ch - 'a'] = y;
-            }
+            p = node[p].link;
+        }
+        if (p == 0) {
+            node[u].link = root;
+            return;
+        }
+        int q = node[p].ch[ch - 'a'];
+        if (node[p].len + 1 == node[q].len) {
+            node[u].link = q;
+            return;
+        }
+        int cq = clone_new_node(q);
+        node[cq].len = node[p].len + 1;
+        node[q].link = node[u].link = cq;
+        while (p > 0 && node[p].ch[ch - 'a'] == q) {
+            node[p].ch[ch - 'a'] = cq;
+            p = node[p].link;
         }
     }
 
-    vector<int> d, rk;
-
-    void update_link() {
+    void dp_on_link_tree() {
         int siz = node.size() - 1;
-        d.clear(), d.resize(siz + 2);
-        rk.clear(), rk.resize(siz + 2);
-
-        for (int i = 1; i <= siz; ++i) ++d[node[i].len];
-        for (int i = 1; i <= siz; ++i) d[i] += d[i - 1];
-        for (int i = 1; i <= siz; ++i) rk[d[node[i].len]--] = i;
-
+        vector<int> cnt_len(siz + 2), rk(siz + 2);
+        for (int i = 1; i <= siz; ++i) ++cnt_len[node[i].len];
+        for (int i = 1; i <= siz; ++i) cnt_len[i] += cnt_len[i - 1];
+        for (int i = 1; i <= siz; ++i) rk[cnt_len[node[i].len]--] = i;
         for (int i = siz; i >= 1; --i) {
-            int u = rk[i], v = node[u].link;  // 对u进行更新则达到了从底向上更新
+            int u = rk[i], v = node[u].link;
             node[v].cnt += node[u].cnt;
         }
     }
 
     ll answer1() {
-        update_link();
-        ll ans = 0;
+        dp_on_link_tree();
+        ll ans = 0LL;
         int siz = node.size() - 1;
-        for (int i = siz; i >= 1; --i) {
-            if (node[i].cnt > 1)
-                ans = max(ans, 1ll * node[i].cnt * node[i].len);
+        for (int i = 1; i <= siz; ++i) {
+            if (node[i].cnt > 1LL) {
+                ans = max(ans, 1LL * node[i].cnt * node[i].len);
+            }
         }
         return ans;
     }
@@ -208,7 +209,7 @@ void purin_init() {}
 void purin_solve() {
     RD(s);
     n = strlen(s + 1);
-    sam.init();
+    sam.init(n);
     for (int i = 1; i <= n; ++i) {
         sam.extend(s[i]);
     }
