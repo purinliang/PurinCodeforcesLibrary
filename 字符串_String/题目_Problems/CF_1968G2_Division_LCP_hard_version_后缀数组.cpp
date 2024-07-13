0c7b5c8dd6405cf262db80afbe7296ce7ce9f1b7
purinliang <purinliang@gmail.com>
2024-07-13 09:41:37 +0800

    Move 字符串 题目

---
+++ 字符串_String/题目_Problems/CF_1968G2_Division_LCP_hard_version_后缀数组.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+#ifdef LOCAL
+#include ".\标准本地调试_StandardLocalDebug.h"
+#else
+#define D(...)
+#define DN(arr, n)
+#define DF()
+#define ASSERT(x)
+
+template <typename T>
+void _RD(T& var) {
+    cin >> var;
+}
+
+void _RD(char* var) {
+    string str;
+    cin >> str;
+    strcpy(var + 1, str.c_str());
+}
+
+void RD() {}
+
+template <typename T, typename... U>
+void RD(T& Head, U&... Tail) {
+    _RD(Head);
+    RD(Tail...);
+}
+
+template <typename T>
+void RDN(T* arr, int n) {
+    for (int i = 1; i <= n; ++i) {
+        _RD(arr[i]);
+    }
+}
+
+template <typename T>
+void _WT(const T& var) {
+    cout << var;
+}
+
+void _WT(const char* var) {
+    cout << (var + 1);
+}
+
+void WT() {}
+
+template <typename T, typename... U>
+void WT(const T& Head, const U&... Tail) {
+    _WT(Head);
+    cout << (sizeof...(Tail) ? " " : "\n");
+    WT(Tail...);
+}
+
+void WTY(bool var, bool capital = false) {
+    if (capital) {
+        cout << (var ? "YES" : "NO") << "\n";
+    } else {
+        cout << (var ? "Yes" : "No") << "\n";
+    }
+}
+
+template <typename T>
+void WTN(T* arr, int n) {
+    for (int i = 1; i <= n; ++i) {
+        _WT(arr[i]);
+        cout << (i < n ? " " : "\n");
+    }
+}
+
+#endif
+
+void purin_ios() {
+    cout << fixed << setprecision(12);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+#define endl "\n"
+#define fout fflush(stdout)
+}
+
+void purin_init();
+void purin_solve();
+
+void purin_online_test(bool ignore_test_case_count) {
+    purin_ios();
+    purin_init();
+    if (!ignore_test_case_count) {
+        int t = 1;
+        cin >> t;
+        for (int i = 1; i <= t; ++i) {
+            purin_solve();
+        }
+    } else {
+        auto cin_eof = [&]() -> bool {
+            char ch;
+            while (cin >> ch) {
+                if (!isspace(ch)) {
+                    cin.unget();
+                    return false;
+                }
+            }
+            return cin.eof();
+        };
+        while (!cin_eof()) {
+            purin_solve();
+        }
+    }
+}
+
+/* MY CODE BEGIN */
+
+const int INF = 0x3F3F3F3F;
+const ll LINF = 0x3F3F3F3F3F3F3F3FLL;
+
+const int MAXN = 2e5 + 10;
+int n, l, r;
+char s[MAXN];
+
+void purin_init() {}
+
+void purin_solve() {
+    RD(n, l, r);
+    RD(s);
+    // TODO 这道题是求出现最少k∈[l, r]次的LCP，
+    // 与LCP有关的问题后缀数组都可以上班了
+    // 出现k次那就设一个长度为k的滑动窗口，在后缀数组上
+    // 滑动，并取height数组的区间最小值
+    // 但是这道题要求分割的字符串不重叠所以还是不能变成模板题
+}
+
+int main() {
+    const bool ignore_test_case_count = false;
+#ifdef LOCAL
+    purin_local_test(ignore_test_case_count);
+#else
+    purin_online_test(ignore_test_case_count);
+#endif
+    return 0;
+}
