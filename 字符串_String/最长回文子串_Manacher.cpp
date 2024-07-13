52bc406277fc061b96004ad599b3586097cb5838
purinliang <purinliang@gmail.com>
2024-07-13 23:26:18 +0800

    Update 最长回文子串_Manacher.cpp

--- 字符串_String/最长回文子串_Manacher.cpp
+++ 字符串_String/最长回文子串_Manacher.cpp
@@ -8,7 +8,10 @@ struct Manacher {
     vector<int> len;
 
     // 字符串 s，1-index，首字符是 s[1] 的字符串；n 字符串长度
-    void init(char* s, int n) {
+    void init(char* s, int n = -1) {
+        if (n == -1) {
+            n = strlen(s + 1);
+        }
         len.clear(), len.resize(2 * n + 2);
         // 最右回文串的位置为 [lm, rm]
         int lm = 0, rm = -1;
@@ -35,13 +38,20 @@ struct Manacher {
     }
 
     // 所有满足 i == l + r 的子串 [l, r] 中的最长回文串的长度
-    int max_length_palindrome(int i) {
+    int longest_palindrome_length(int i) {
         return 2 * len[i] - (i % 2 == 0);
+    }
+
+    // 所有满足 i == l + r 的子串 [l, r] 中的最长回文串的范围
+    pair<int, int> longest_palindrome_range(int i) {
+        int lmid = i / 2, rmid = i - lmid;
+        int l = lmid - len[i] + 1, r = rmid + len[i] - 1;
+        return {l, r};  // longest_palindrome_length(i) == r - l + 1
     }
 
     // 判断子串 [l, r] 是否为回文串
     bool is_palindrome(int l, int r) {
-        return max_length_palindrome(l + r) >= (r - l + 1);
+        return longest_palindrome_length(l + r) >= (r - l + 1);
     }
 
 } manacher;
